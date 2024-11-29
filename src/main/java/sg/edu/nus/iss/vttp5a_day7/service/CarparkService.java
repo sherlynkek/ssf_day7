e2dd068c3b3beb60cbaa2e1d4d90cb1cbda3bee3
sherlynkek <sherlynkek00@gmail.com>
2024-11-29 14:27:33 +0800

    clear

--- src/main/java/sg/edu/nus/iss/vttp5a_day7/service/CarparkService.java
+++ src/main/java/sg/edu/nus/iss/vttp5a_day7/service/CarparkService.java
@@ -13,20 +13,6 @@ import sg.edu.nus.iss.vttp5a_day7.constant.Url;
 import sg.edu.nus.iss.vttp5a_day7.model.Carpark;
 import sg.edu.nus.iss.vttp5a_day7.repo.ListRepo;
 
-/* @Service
-public class CarparkService {
-
-    @Autowired
-    ListRepo carparkRepo;
-
-    RestTemplate restTemplate = new RestTemplate();
-
-    public List<Carpark> getCarparkList() {
-        ResponseEntity<List<Carpark>> carparks = restTemplate.exchange(Url.carparkUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<Carpark>>() {});
-
-        return carparks.getBody();
-    }
-} */
 @Service
 public class CarparkService {
     
